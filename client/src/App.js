bf59472e5e993e2780b77acf47d1734737af3e76
vinayakbanga <vinayakbanga22@gmail.com>
2021-10-11 23:28:57 +0530

    food tab added

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import HomeLayoutHOC from "./HOC/home.hoc";
 // components
 
 import Temp from "./components/temp";
+import Master from "./components/master";
 
 
 function App() {
  return (
  <>
  <HomeLayoutHOC path="/" exact component={Temp}/>
+ <HomeLayoutHOC path="/:type" exact component={Master}/>
  </>
  )
  
--- client/src/Layout/Home.layout.js
+++ client/src/Layout/Home.layout.js
@@ -2,15 +2,18 @@ import React from "react";
 
 //components
 import Navbar from "../components/Navbar";
+import FoodTab from "../components/FoodTab";
 
 const HomeLayout =(props)=>{
     return <>
         
     <div className="container mx-auto px-4 lg:px-20">
     <Navbar />
+    
 
     {props.children}
     </div>
+    <FoodTab/>
     </>
 };
 export default HomeLayout;
